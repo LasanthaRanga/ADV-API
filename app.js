a8964565d951f67f758c78eaf5ee5c9e75fe0fdd
Ranga Rathnayake <35551906+LasanthaRanga@users.noreply.github.com>
2020-08-26 02:05:58 +0530

    update subbox.lk cors

--- app.js
+++ app.js
@@ -26,10 +26,9 @@ const allowedOrigins = [
   'http://localhost:4200',
   'http://localhost:8080',
   'http://localhost:8100',
-  'http://test.coopshop.lk',
-  'https://test.coopshop.lk',
-  'https://coopshop.lk',
-  'https://adv.codetechasia.com'
+  'http://supbox.lk',  
+  'https://supbox.lk',
+  'https://adv.codetechasia.com', 
 
 ];
 
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,6 @@ router.get('/', function(req, res, next) {
   res.send('index', { title: 'Express' });
 });
 
+// testing comit
+
 module.exports = router;
