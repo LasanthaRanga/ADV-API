5a3057200cdf308e3b97ab7915cfcb66f86080bb
Ranga Rathnayake <35551906+LasanthaRanga@users.noreply.github.com>
2020-12-07 16:33:53 +0530

    updates

--- app.js
+++ app.js
@@ -28,9 +28,10 @@ const allowedOrigins = [
   'http://localhost:4200',
   'http://localhost:8080',
   'http://localhost:8100',
+  'http://supbox.lk',  
   'https://supbox.lk',
   'https://probox.lk',
-  'https://proboxapi.probox.lk',
+  'http://probox.lk',
   'https://adv.codetechasia.com',
 
 ];
