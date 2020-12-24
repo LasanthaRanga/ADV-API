d6cc3fbd1337a962ac9605a3738667c061ac52e0
Ranga Rathnayake <35551906+LasanthaRanga@users.noreply.github.com>
2020-12-24 19:43:36 +0530

    upload

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ const allowedOrigins = [
   'http://localhost:4200',
   'http://localhost:8080',
   'http://localhost:8100',
+  'https://test.probox.lk',
   'http://supbox.lk',  
   'https://supbox.lk',
   'https://probox.lk',
