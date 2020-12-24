b70820677f78188f4b2e8f0b82ef4ef640f5bd84
Ranga Rathnayake <35551906+LasanthaRanga@users.noreply.github.com>
2020-12-24 19:40:52 +0530

    remove test croa

--- app.js
+++ app.js
@@ -32,8 +32,6 @@ const allowedOrigins = [
   'https://supbox.lk',
   'https://probox.lk',
   'http://probox.lk',
-  'http://test.probox.lk',
-  'https://test.probox.lk',
   'https://adv.codetechasia.com',
 
 ];
